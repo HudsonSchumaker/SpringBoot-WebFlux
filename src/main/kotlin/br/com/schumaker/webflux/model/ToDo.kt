9c8b98d074aa66e51e620ce58768b2b5e7316981
Hudson Schumaker <hudson.schumaker@me.com>
2021-09-22 20:43:02 +0200

    Commit;

---
+++ src/main/kotlin/br/com/schumaker/webflux/model/ToDo.kt
@@ -0,0 +1,31 @@
+package br.com.schumaker.webflux.model
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
+import java.util.*
+
+@Document
+class ToDo {
+
+    @Id
+    var id: String? = null
+    var description: String = ""
+    var created: LocalDateTime = LocalDateTime.now()
+    var modified: LocalDateTime = LocalDateTime.now()
+    var completed: Boolean = false
+
+    constructor() {
+    }
+
+    constructor(description: String) {
+        this.id = UUID.randomUUID().toString()
+        this.description = description
+    }
+
+    constructor(description: String, completed: Boolean) {
+        this.id = UUID.randomUUID().toString()
+        this.description = description
+        this.completed = completed
+    }
+}
