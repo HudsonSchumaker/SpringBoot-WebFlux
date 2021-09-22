586223c46bc3fb202b23b76f67d6b8fa98f27582
Hudson Schumaker <hudson.schumaker@me.com>
2021-09-22 20:04:40 +0200

    Commit;

---
+++ src/main/kotlin/br/com/schumaker/webflux/WebfluxApplication.kt
@@ -0,0 +1,11 @@
+package br.com.schumaker.webflux
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebfluxApplication
+
+fun main(args: Array<String>) {
+	runApplication<WebfluxApplication>(*args)
+}
