704fbec001c84b4c6daa47622e82c83da15ce356
Hudson Schumaker <hudson.schumaker@me.com>
2021-09-22 21:17:47 +0200

    Commit;

--- src/main/kotlin/br/com/schumaker/webflux/WebfluxApplication.kt
+++ src/main/kotlin/br/com/schumaker/webflux/WebfluxApplication.kt
@@ -1,10 +1,22 @@
 package br.com.schumaker.webflux
 
+import br.com.schumaker.webflux.model.ToDo
+import br.com.schumaker.webflux.model.TodoRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
-class WebfluxApplication
+class WebfluxApplication {
+	@Bean
+	fun run(repository: TodoRepository) = CommandLineRunner {
+
+		repository.save(
+			ToDo("Test")
+		).subscribe()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<WebfluxApplication>(*args)
