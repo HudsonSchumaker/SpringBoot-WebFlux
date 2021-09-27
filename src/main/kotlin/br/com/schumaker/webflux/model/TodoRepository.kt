45f94b8acb53f9293769f9c178d929cf129d587a
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-09-27 21:27:24 +0200

    Update TodoRepository.kt

--- src/main/kotlin/br/com/schumaker/webflux/model/TodoRepository.kt
+++ src/main/kotlin/br/com/schumaker/webflux/model/TodoRepository.kt
@@ -3,6 +3,10 @@ package br.com.schumaker.webflux.model
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 
+/**
+ *
+ * @author hudson.schumaker
+ */
 @Repository
 interface TodoRepository: ReactiveMongoRepository<ToDo, String> {
 }
