c4b5cb0def6442491dfcc1701d89d281ba3decee
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2021-09-27 21:27:11 +0200

    Update ToDo.kt

--- src/main/kotlin/br/com/schumaker/webflux/model/ToDo.kt
+++ src/main/kotlin/br/com/schumaker/webflux/model/ToDo.kt
@@ -5,6 +5,10 @@ import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 import java.util.*
 
+/**
+ *
+ * @author hudson.schumaker
+ */
 @Document
 class ToDo {
 
